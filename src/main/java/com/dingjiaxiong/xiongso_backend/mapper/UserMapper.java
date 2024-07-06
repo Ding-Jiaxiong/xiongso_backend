b402d9d468afd19ec1777f097c1dc97636aa67b5
DingJiaxiong <1612102142@qq.com>
2024-07-06 18:08:30 +0800

    后端初始化 + 仓库初始化

---
+++ src/main/java/com/dingjiaxiong/xiongso_backend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.dingjiaxiong.xiongso_backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dingjiaxiong.xiongso_backend.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author Ding Jiaxiong
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
